0879921a6ae86792eab53b5295b82b0a87a14fb6
nachiketbariya1311-creator <nachiketbariya1311@gmail.com>
2026-08-09 19:44:14 +0530

    Time: 3 ms (94.5%), Space: 44.8 MB (48.91%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -4,18 +4,27 @@ public:
         int v=min(g.size(),s.size());
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int count=0;
+    //    int count=0;
         int i=0,j=0;
-    while(i<g.size() && j<s.size()){
-        if(s[j]>=g[i]){
-          count++;
-          i++;
-          j++;
-        }
-        else{
+    // while(i<g.size() && j<s.size()){
+    //     if(s[j]>=g[i]){
+    //       count++;
+    //       i++;
+    //       j++;
+    //     }
+    //     else{
+    //         j++;
+    //     }
+    // }
+    // return count;
+    while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) {
+                i++;
+            }
             j++;
         }
-    }
-      return count;
+
+        return i;
+
     }
 };
