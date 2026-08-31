14f55540439ccf0bdf5c36328a9aeb967f6c3020
nachiketbariya1311-creator <nachiketbariya1311@gmail.com>
2026-08-31 22:01:16 +0530

    Time: 64 ms (34.61%), Space: 98.5 MB (52.68%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool check(long long mid ,vector<int>& time, int totalTrips){
-        long long trips=0;
-        for(int i=0;i<time.size();i++){
-            trips+=(mid/(long long)time[i]);
-        }
-        if(trips<(long long)totalTrips) return false;
-        else return true;
-    }
+    // bool check(long long mid ,vector<int>& time, int totalTrips){
+    //     long long trips=0;
+    //     for(int i=0;i<time.size();i++){
+    //         trips+=(mid/(long long)time[i]);
+    //     }
+    //     if(trips<(long long)totalTrips) return false;
+    //     else return true;
+    // }
     long long minimumTime(vector<int>& time, int totalTrips) {
         int mx=0;
         for (int i=0;i<time.size();i++){
@@ -18,7 +18,11 @@ public:
         long long ans=-1;
         while(lo<=hi){
             long long mid= lo +(hi-lo)/2;
-            if(check(mid,time,totalTrips)==true){
+            long long trips=0;
+        for(int i=0;i<time.size();i++){
+            trips+=(mid/(long long)time[i]);
+        }
+            if(trips>=totalTrips){
                 hi=mid-1;
                 ans=mid;
             }
