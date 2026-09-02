13c6538352c553a8d9538ab56ab85e5316ff6fcc
nachiketbariya1311-creator <nachiketbariya1311@gmail.com>
2026-09-02 20:33:31 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.57%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -6,70 +6,33 @@ public:
         int i=n-1;
         while(i>=0){
             int o=0;
-            if(s[i]=='I') {
-                o=1;
-                i--;
-            }    
+            if(s[i]=='I') o=1;   
             else if(s[i]=='V') {
-                if(i!=0 && s[i-1]=='I')  {
-                    o=4;
-                    i-=2;
-                }
-                else {
-                    o=5;
-                    i--;
-                }    
+                if(i!=0 && s[i-1]=='I') o=4;
+                else o=5;
             }
             else if(s[i]=='X') {
-                if(i!=0 && s[i-1]=='I'){
-                    o=9;
-                    i-=2;
-                }   
-                else {
-                    o=10;
-                    i--;
-                }
+                if(i!=0 && s[i-1]=='I') o=9;   
+                else o=10;
             }
             else if(s[i]=='L') {
-                if(i!=0 && s[i-1]=='X'){
-                    o=40;
-                    i-=2;
-                }  
-                else{
-                    o=50;
-                    i--;
-                } 
+                if(i!=0 && s[i-1]=='X') o=40;
+                else o=50;
             }
             else if(s[i]=='C') {
-                if(i!=0 && s[i-1]=='X'){
-                    o=90;
-                    i-=2;
-                }   
-                else{
-                    o=100;
-                    i--;
-                } 
+                if(i!=0 && s[i-1]=='X') o=90;
+                else o=100;
             }
             else if(s[i]=='D') {
-                if(i!=0 && s[i-1]=='C'){
-                    o=400;
-                    i-=2;
-                }   
-                else{
-                    o=500;
-                    i--;
-                } 
+                if(i!=0 && s[i-1]=='C') o=400;  
+                else o=500;
             }
             else if(s[i]=='M'){
-                if(i!=0 && s[i-1]=='C'){
-                    o=900;
-                    i-=2;
-                }   
-                else{
-                    o=1000;
-                    i--;
-                } 
-            } 
+                if(i!=0 && s[i-1]=='C') o=900;   
+                else o=1000;
+            }
+            if(o==4||o==9||o==40||o==90||o==400||o==900)  i-=2;
+            else i--; 
             ans+=o;
         }
         return ans;
