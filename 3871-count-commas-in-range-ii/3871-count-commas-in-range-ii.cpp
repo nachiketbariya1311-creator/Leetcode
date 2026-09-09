008561fa8f82689fc96e2950edbed33240ecd50d
nachiketbariya1311-creator <nachiketbariya1311@gmail.com>
2026-09-09 23:34:13 +0530

    Time: 1 ms (8.53%), Space: 9.2 MB (19.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-    
-    if(n<=999999 && n>999) return n-999;
-    else if(n>999999 && n<=999999999) return 2*(n-999999)+999000;
-    else if(n>999999999 && n<=999999999999) return 3*(n-999999999)+(2*999000000)+(999000);
-    else if(n>999999999999 && n<=999999999999999) return 4*(n-999999999999)+(3*999000000000)+(2*999000000)+(999000);
-    else if(n>999999999999999) return 5+(4*999000000000000)+(3*999000000000)+(2*999000000)+(999000);
-    return 0;
+        long long p=1000;
+        long long ans=0;
+        while(p<=n){
+            ans+=n-p+1;
+            if(n<=p/1000){
+                break;
+            }
+            p*=1000;
+        }
+        return ans;
     }
 };
