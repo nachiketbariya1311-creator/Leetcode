54cd1f7a33e5c6d6e78275e6454b36a50c205b07
nachiketbariya1311-creator <nachiketbariya1311@gmail.com>
2026-09-10 22:43:43 +0530

    Time: 4 ms (74.33%), Space: 9.9 MB (98.24%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dayOfYear(string date) {
+        int y=(date[0]-'0')*1000+(date[1]-'0')*100+(date[2]-'0')*10+(date[3]-'0');
+        int m=(date[5]-'0')*10+(date[6]-'0');
+        int ans=(date[8]-'0')*10+(date[9]-'0');
+        if(m==1) return ans;
+        for(int i=1;i<m;i++){
+            if(i==2) {
+                if(y%100==0){
+                    if(y%400==0) ans+=29;
+                    else ans+=28;
+                } 
+                else if(y%4==0) ans+=29;
+                else  ans+=28;
+            }
+            else if(i==4||i==6||i==9|| i==11) ans+=30;
+            else ans+=31;
+        }
+        return ans;
+    }
+};
