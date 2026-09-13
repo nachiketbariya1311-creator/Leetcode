66c432045c83b8fcbf8710cc030f6bf411db1792
nachiketbariya1311-creator <nachiketbariya1311@gmail.com>
2026-09-13 18:18:36 +0530

    Time: 0 ms (100%), Space: 7.9 MB (3.58%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     bool isThree(int n) {
-        int k=0;
-        int i=1;
-        while(i<=n){
-            if(n%i==0) k++;
-            i++;
-        }
-        if(k==3) return true;
+        // int k=0;
+        // int i=1;
+        // while(i<=n){
+        //     if(n%i==0) k++;
+        //     i++;
+        // }
+        // if(k==3) return true;
+        // return false;
+
+    int root = sqrt(n);
+
+    if (root * root != n)
         return false;
-        
+
+    for (int i = 2; i * i <= root; i++) {
+        if (root % i == 0)
+            return false;
     }
+
+    return root > 1;
+   }
 };
